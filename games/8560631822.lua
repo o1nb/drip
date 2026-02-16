6d72804f450123ba0d3ff3022d46b3964221bc6e
o1nb <hogrida6222@gmail.com>
2026-02-15 18:14:25 -0800

    Update 8560631822.lua

--- games/8560631822.lua
+++ games/8560631822.lua
@@ -15,7 +15,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function() 
-			return game:HttpGet('https://raw.githubusercontent.com/o1nb/drip/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true) 
+			return game:HttpGet('https://raw.githubusercontent.com/o1nb/drip/'..readfile('Drip X/profiles/commit.txt')..'/'..select(1, path:gsub('Drip X/', '')), true) 
 		end)
 		if not suc or res == '404: Not Found' then 
 			error(res) 
@@ -29,15 +29,15 @@ local function downloadFile(path, func)
 end
 
 vape.Place = 6872274481
-if isfile('newvape/games/'..vape.Place..'.lua') then
-	loadstring(readfile('newvape/games/'..vape.Place..'.lua'), 'bedwars')()
+if isfile('Drip X/games/'..vape.Place..'.lua') then
+	loadstring(readfile('Drip X/games/'..vape.Place..'.lua'), 'bedwars')()
 else
 	if not shared.VapeDeveloper then
 		local suc, res = pcall(function() 
-			return game:HttpGet('https://raw.githubusercontent.com/o1nb/drip/'..readfile('newvape/profiles/commit.txt')..'/games/'..vape.Place..'.lua', true) 
+			return game:HttpGet('https://raw.githubusercontent.com/o1nb/drip/'..readfile('Drip X/profiles/commit.txt')..'/games/'..vape.Place..'.lua', true) 
 		end)
 		if suc and res ~= '404: Not Found' then
-			loadstring(downloadFile('newvape/games/'..vape.Place..'.lua'), 'bedwars')()
+			loadstring(downloadFile('Drip X/games/'..vape.Place..'.lua'), 'bedwars')()
 		end
 	end
 end
