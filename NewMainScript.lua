93b9649731d98cff4f10e22ff0d00acb555e16d6
o1nb <hogrida6222@gmail.com>
2026-02-15 18:13:49 -0800

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -11,7 +11,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('Drip X/profiles/commit.txt')..'/'..select(1, path:gsub('Drip X/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -34,7 +34,7 @@ local function wipeFolder(path)
 	end
 end
 
-for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
+for _, folder in {'Drip X', 'Drip X/games', 'Drip X/profiles', 'Drip X/assets', 'Drip X/libraries', 'Drip X/guis'} do
 	if not isfolder(folder) then
 		makefolder(folder)
 	end
@@ -47,13 +47,13 @@ if not shared.VapeDeveloper then
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
 	commit = commit and #commit == 40 and commit or 'main'
-	if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
-		wipeFolder('newvape')
-		wipeFolder('newvape/games')
-		wipeFolder('newvape/guis')
-		wipeFolder('newvape/libraries')
+	if commit == 'main' or (isfile('Drip X/profiles/commit.txt') and readfile('Drip X/profiles/commit.txt') or '') ~= commit then
+		wipeFolder('Drip X')
+		wipeFolder('Drip X/games')
+		wipeFolder('Drip X/guis')
+		wipeFolder('Drip X/libraries')
 	end
-	writefile('newvape/profiles/commit.txt', commit)
+	writefile('Drip X/profiles/commit.txt', commit)
 end
 
-return loadstring(downloadFile('newvape/main.lua'), 'main')()
+return loadstring(downloadFile('Drip X/main.lua'), 'main')()
